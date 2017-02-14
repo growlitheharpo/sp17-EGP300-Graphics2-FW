7557bdafdcea1ca50ee9662eda5799a1c355cebb
James Keats <growlitheharpo@gmail.com>
2017-02-13 20:56:56 -0500

    Fill in the passthrough shader

--- EGP Graphics FW Class Base/resource/glsl/4x/vs/passTexcoord_passthruPosition_vs4x.glsl
+++ EGP Graphics FW Class Base/resource/glsl/4x/vs/passTexcoord_passthruPosition_vs4x.glsl
@@ -13,17 +13,26 @@
 // ****
 // attributes
 
+layout (location = 0) in vec4 position;
+layout (location = 8) in vec4 texcoord;
+
 
 // ****
 // varyings
 
+out v2f
+{
+	vec2 texcoord;
+} passData;
 
 // shader function
 void main()
 {
 	// ****
 	// copy position attribute directly to GL's clip position
+	gl_Position = position;
 
 	// ****
 	// pass data
+	passData.texcoord = texcoord.xy;
 }
