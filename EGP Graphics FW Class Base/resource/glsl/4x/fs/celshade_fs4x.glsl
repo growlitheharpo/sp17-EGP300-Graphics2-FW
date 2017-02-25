c5c9cfe826bc2adfaacceb526a92a2484cfc3589
James Keats <james.keats@mymail.champlain.edu>
2017-02-25 16:04:55 -0500

    Improve the fragment shader

--- EGP Graphics FW Class Base/resource/glsl/4x/fs/celshade_fs4x.glsl
+++ EGP Graphics FW Class Base/resource/glsl/4x/fs/celshade_fs4x.glsl
@@ -33,6 +33,9 @@ layout (location = 0) out vec4 fragColor;
 
 
 const vec4 AMBIENT = vec4(0.08, 0.04, 0.02, 1.0);
+const float INTENSITY = 0.6;
+
+const vec4 BASE = vec4(0.0, 0.2, 1.0, 1.0);
 
 // shader function
 void main()
@@ -41,12 +44,14 @@ void main()
 	// 1. diffuse
 	vec4 N = normalize(pass.normal);
 	vec4 L = normalize(pass.lightVec);
-	float kd = dot(N, L);
-
-	// 3. clamp & exponent
-	kd = max(0.0, kd);
-
-	vec4 texColor = (texture(tex_sm, vec2(kd, 0.5)) * (kd * 0.8 + 0.2));
 	
-	fragColor = texture(tex_dm, pass.texcoord.xy) * texColor;
+	//Lighting coord
+	float kd = max(0.0, dot(N, L));
+
+	vec4 texColor = texture(tex_sm, vec2(kd, 0.5));
+
+	texColor = texColor * (kd * INTENSITY + (1.0 - INTENSITY));
+	
+	fragColor = texColor * texture(tex_dm, pass.texcoord.xy);
+	fragColor = texColor * BASE;
 }
