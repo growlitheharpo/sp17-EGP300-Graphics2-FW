3b19d4af771754726bdb719854107aca3a7d46fb
James Keats <james.keats@mymail.champlain.edu>
2017-03-07 17:55:14 -0500

    Add a very simple render path.

---
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/RenderPath.cpp
@@ -0,0 +1,28 @@
+﻿#include "RenderPath.h"
+
+RenderPath::RenderPath()
+{
+}
+
+RenderPath::~RenderPath()
+{
+}
+
+void RenderPath::addRenderPass(const RenderPass& pass)
+{
+	mPasses.push_back(pass);
+}
+
+void RenderPath::addRenderPass(RenderPass&& pass)
+{
+	mPasses.push_back(std::move(pass));
+}
+
+void RenderPath::doRender(renderFuncPtr func, const cbmath::mat4* mvp_main, const cbmath::mat4* mvp_other, const cbmath::mat4* atlas, const unsigned numOtherMatrices, const unsigned numSceneObjects, int lighting, int cullBack)
+{
+	for (auto pass : mPasses)
+	{
+		pass.activate();
+		(*func)(mvp_main, mvp_other, atlas, numOtherMatrices, numSceneObjects, lighting, cullBack);
+	}
+}
---
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/RenderPath.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <vector>
+#include "RenderPass.h"
+
+namespace cbmath
+{
+	union mat4;
+}
+
+class RenderPath
+{
+	public:
+		typedef void(*renderFuncPtr)(const cbmath::mat4*, const cbmath::mat4*, const cbmath::mat4*, const unsigned int, const unsigned int, int, int);
+
+	private:
+		std::vector<RenderPass> mPasses;
+
+	public:
+		RenderPath();
+		~RenderPath();
+
+		void addRenderPass(const RenderPass& pass);
+		void addRenderPass(RenderPass&& pass);
+
+		void doRender(renderFuncPtr renderFunc, const cbmath::mat4 *mvp_main, const cbmath::mat4 *mvp_other, const cbmath::mat4 *atlas, const unsigned int numOtherMatrices, const unsigned int numSceneObjects, int lighting, int cullBack);
+};
