e57260c7988a03a89b32bdaf54cf54b17d9acfe6
James Keats <james.keats@mymail.champlain.edu>
2017-03-06 20:39:59 -0500

    Add the basic projection vertex and fragment shader

--- EGP Graphics FW Class Base/resource/glsl/4x/fs_shadow/phong_projtex_fs4x.glsl
+++ EGP Graphics FW Class Base/resource/glsl/4x/fs_shadow/phong_projtex_fs4x.glsl
@@ -11,11 +11,25 @@
 
 // ****
 // varyings: 
+in vertexdata
+{
+	vec4 position_clip_alt;
+	vec4 normal;
+	vec4 texcoord;
+	vec4 lightVec;
+	vec4 eyeVec;
+} pass;
 
 
 // ****
 // uniforms: 
+uniform sampler2D tex_dm;
+uniform sampler2D tex_sm;
 
+uniform sampler2D tex_bool;
+uniform sampler2D projtex;
+
+uniform vec4 lightColor;
 
 // target: 
 layout (location = 0) out vec4 fragColor;
@@ -23,9 +37,37 @@ layout (location = 0) out vec4 fragColor;
 
 // ****
 // constants/globals: 
-
+const vec4 AMBIENT = vec4(0.02, 0.01, 0.0, 0.0);
+vec2 booleanSample = vec2(0, 0);
 
 void main()
 {
-	fragColor = vec4(1.0, 0.5, 0.0, 1.0);
+	//adjust for perspective!!
+	vec4 projector_uv = pass.position_clip_alt / pass.position_clip_alt.w;
+
+	vec4 projColor = texture(projtex, projector_uv.xy);
+
+	vec4 diffuseColor = texture(tex_dm, pass.texcoord.xy);
+	vec4 specColor = texture(tex_sm, pass.texcoord.xy);
+
+	vec4 N = normalize(pass.normal);
+	vec4 L = normalize(pass.lightVec);
+	float kd = dot(N, L);
+
+	// 2. specular
+	vec4 V = normalize(pass.eyeVec);
+	vec4 R = (kd+kd)*N - L;
+	float ks = dot(V, R);
+
+	// 3. clamp & exponent
+	kd = max(0.0, kd);
+	ks = max(0.0, ks);
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+	ks *= ks;
+
+	fragColor = (diffuseColor * kd + specColor * ks) * lightColor * projColor + AMBIENT;
 }
