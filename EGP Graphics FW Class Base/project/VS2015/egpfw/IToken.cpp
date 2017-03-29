2d4ee66157dea3cb4553327431d86f25d4064ad8
James Keats <james.keats@mymail.champlain.edu>
2017-03-28 20:52:27 -0400

    Start working on the token classes.

---
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/IToken.cpp
@@ -0,0 +1,74 @@
+﻿#include "IToken.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void SymbolToken::parseToken(fstream& fin)
+{
+	fin.seekg(-1, ios_base::cur); //move back 1 character
+	char currentChar;
+
+	do
+	{
+		currentChar = fin.get();
+		mSymbol += currentChar;
+
+		currentChar = fin.peek();
+	} while (isalnum(currentChar) || currentChar == '_');
+}
+
+void SymbolToken::debugPrint() const
+{
+	cout << "Token: [symbol, \"" << mSymbol << "\"]";
+}
+
+void WhitespaceToken::parseToken(std::fstream& fin)
+{
+
+}
+
+void WhitespaceToken::debugPrint() const
+{
+	cout << "Token: [whitespace, \"" << mWhitespace << "\"]";
+}
+
+void PunctuationToken::parseToken(std::fstream& fin)
+{
+
+}
+
+void PunctuationToken::debugPrint() const
+{
+	cout << "Token: [punctuation, \"" << mPunctuation << "\"]";
+}
+
+void NumberLiteralToken::parseToken(std::fstream& fin)
+{
+
+}
+
+void NumberLiteralToken::debugPrint() const
+{
+	cout << "Token: [number_literal, \"" << mNumber << "\"]";
+}
+
+void EOLToken::parseToken(std::fstream& fin)
+{
+
+}
+
+void EOLToken::debugPrint() const
+{
+	cout << "Token: [end_of_line, \"" << "" << "\"]";
+}
+
+void EOFToken::parseToken(std::fstream& fin)
+{
+
+}
+
+void EOFToken::debugPrint() const
+{
+	cout << "Token: [end_of_file, \"" << "" << "\"]";
+}
---
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/IToken.h
@@ -0,0 +1,106 @@
+/***************************************************************************************** /
+ *
+ * Many, many many thanks to the author of the following post, which was instrumental
+ * in even just getting started with this project.
+ * http://www.dreamincode.net/forums/topic/153718-fundamentals-of-parsing/
+ * 
+ ******************************************************************************************/
+#pragma once
+#include <fstream>
+
+class IToken
+{
+	public:
+		enum Type
+		{
+			INVALID = 0,
+			SYMBOL,
+			WHITESPACE,
+			PUNCTUATION,
+			NUMBER_LITERAL,
+			END_OF_LINE,
+			END_OF_FILE,
+		};
+
+	private:
+		Type mType;
+
+	public:
+		explicit IToken(Type t) : mType(t) {}
+		virtual ~IToken() = default;
+
+		virtual void parseToken(std::fstream& fin) = 0;
+		virtual void debugPrint() const = 0;
+};
+
+class SymbolToken : public IToken
+{
+	private:
+		std::string mSymbol;
+
+	public:
+		SymbolToken() : IToken(SYMBOL) {}
+		~SymbolToken() = default;
+
+		void parseToken(std::fstream& fin) override;
+		void debugPrint() const override;
+};
+
+class WhitespaceToken : public IToken
+{
+	private:
+		std::string mWhitespace;
+
+	public:
+		WhitespaceToken() : IToken(WHITESPACE) {}
+		~WhitespaceToken() = default;
+
+		void parseToken(std::fstream& fin) override;
+		void debugPrint() const override;
+};
+
+class PunctuationToken : public IToken
+{
+	private:
+		std::string mPunctuation;
+
+	public:
+		PunctuationToken() : IToken(PUNCTUATION) {}
+		~PunctuationToken() = default;
+
+		void parseToken(std::fstream& fin) override;
+		void debugPrint() const override;
+};
+
+class NumberLiteralToken : public IToken
+{
+	private:
+		std::string mNumber;
+
+	public:
+		NumberLiteralToken() : IToken(NUMBER_LITERAL) {}
+		~NumberLiteralToken() = default;
+
+		void parseToken(std::fstream& fin) override;
+		void debugPrint() const override;
+};
+
+class EOLToken : public IToken
+{
+	public:
+		EOLToken() : IToken(END_OF_LINE) {}
+		~EOLToken() = default;
+
+		void parseToken(std::fstream& fin) override;
+		void debugPrint() const override;
+};
+
+class EOFToken : public IToken
+{
+	public:
+		EOFToken() : IToken(END_OF_FILE) {}
+		~EOFToken() = default;
+
+		void parseToken(std::fstream& fin) override;
+		void debugPrint() const override;
+};
