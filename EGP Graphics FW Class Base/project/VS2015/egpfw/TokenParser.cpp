a293f3ed47593c279c126f293b01ad43fd826b30
James Keats <james.keats@mymail.champlain.edu>
2017-03-29 21:30:14 -0400

    Add empty token parser.

---
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/TokenParser.cpp
@@ -0,0 +1,20 @@
+﻿#include "TokenParser.h"
+#include "IToken.h"
+
+void TokenParser::emit(const std::string& addition)
+{
+	mOutput += addition;
+}
+
+TokenParser::TokenParser()
+{
+
+}
+
+TokenParser::~TokenParser()
+{
+}
+
+void TokenParser::parseTokens(TokenStream& tokens, int glVersion)
+{
+}
---
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/TokenParser.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+#include "TokenStream.h"
+
+class IToken;
+
+class TokenParser
+{
+	private:
+		std::string mOutput;
+
+		void emit(const std::string& addition);
+		
+	public:
+		TokenParser();
+		~TokenParser();
+
+		void parseTokens(TokenStream& tokens, int glVersion);
+
+		std::string getValue() const { return mOutput; }
+};
