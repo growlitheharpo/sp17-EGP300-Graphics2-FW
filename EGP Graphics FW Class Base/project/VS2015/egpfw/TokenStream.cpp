b83fab0b7edcc44fbd1848b1b45053208576a366
James Keats <james.keats@mymail.champlain.edu>
2017-03-29 21:21:48 -0400

    Add a simple token stream class.

---
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/TokenStream.cpp
@@ -0,0 +1,44 @@
+﻿#include "TokenStream.h"
+
+TokenStream::TokenStream(std::vector<IToken*> const& tokens)
+{
+	mTokens = tokens;
+	mOffset = 0;
+}
+
+TokenStream::TokenStream(std::vector<IToken>&& tokens)
+{
+	std::move(tokens.begin(), tokens.end(), mTokens);
+	mOffset = 0;
+}
+
+IToken* TokenStream::get()
+{
+	if (++mOffset > mTokens.size())
+		throw new std::out_of_range("Stream moved out of range!");
+	
+	return mTokens[mOffset - 1];
+}
+
+IToken* TokenStream::peek()
+{
+	return mTokens[mOffset];
+}
+
+void TokenStream::seek(std::streampos pos, std::ios_base::seek_dir dir)
+{
+	switch (dir)
+	{
+		case std::ios_base::beg:
+			mOffset = pos;
+			break;
+		case std::ios_base::cur:
+			mOffset += pos;
+			break;
+		case std::ios_base::end:
+			mOffset = mTokens.size() - 1 - pos;
+			break;
+		default: 
+			throw std::invalid_argument("Seek direction was invalid!");
+	}
+}
---
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/TokenStream.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <vector>
+#include <ios>
+
+class IToken;
+
+class TokenStream
+{
+	private:
+		std::vector<IToken*> mTokens;
+		size_t mOffset;
+
+	public:
+		TokenStream(std::vector<IToken*> const& tokens);
+		explicit TokenStream(std::vector<IToken>&& tokens);
+
+		IToken* get();
+		IToken* peek();
+
+		void seek(std::streampos pos, std::ios_base::seek_dir dir);
+
+		bool eof() const { return mOffset == mTokens.size(); }
+		bool eos() const { return eof(); }
+		bool atEnd() const { return eof(); }
+};
