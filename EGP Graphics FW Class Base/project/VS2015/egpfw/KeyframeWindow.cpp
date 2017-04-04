a657db95c22eaceeca7c99643bd51c724fcde8ac
James Keats <james.keats@mymail.champlain.edu>
2017-04-03 20:41:14 -0400

    Add comments

--- EGP Graphics FW Class Base/project/VS2015/egpfw/KeyframeWindow.cpp
+++ EGP Graphics FW Class Base/project/VS2015/egpfw/KeyframeWindow.cpp
@@ -6,6 +6,7 @@
 #include "transformMatrix.h"
 #include <GL/freeglut.h>
 
+// Constant array of colors used to draw the lines on the keyframe window
 const std::array<cbmath::vec4, KeyframeWindow::NUM_OF_CHANNELS + 1> COLORS = 
 {
 	cbmath::vec4(1.0f, 0.0f, 0.0f, 1.0f),
@@ -16,20 +17,6 @@ const std::array<cbmath::vec4, KeyframeWindow::NUM_OF_CHANNELS + 1> COLORS =
 	cbmath::vec4(0.0f, 0.0f, 1.0f, 1.0f),
 	cbmath::vec4(1.0f, 1.0f, 1.0f, 1.0f),
 };
-
-void KeyframeWindow::resetKeyframes()
-{
-	cbmath::vec4 zeroVec = cbmath::vec4(0.0f, mWindowSize.y / 2.0f, 0.0f, 1.0f);
-	cbmath::vec4 oneVec = cbmath::vec4(mWindowSize.x, mWindowSize.y / 2.0f, 0.0f, 1.0f);
-
-	for (auto& list : mWaypointChannels)
-	{
-		list.clear();
-
-		list.push_back(zeroVec);
-		list.push_back(oneVec);
-	}
-}
 
 KeyframeWindow::KeyframeWindow(egpVertexArrayObjectDescriptor* vao, egpFrameBufferObjectDescriptor* fbo, egpProgram* programs)
 {
@@ -46,34 +33,52 @@ KeyframeWindow::~KeyframeWindow()
 
 }
 
+void KeyframeWindow::resetKeyframes()
+{
+	//Clear everything, then set a point with an effective y value of 0 at the start and end.
+
+	cbmath::vec4 zeroVec = cbmath::vec4(0.0f, mWindowSize.y / 2.0f, 0.0f, 1.0f);
+	cbmath::vec4 oneVec = cbmath::vec4(mWindowSize.x, mWindowSize.y / 2.0f, 0.0f, 1.0f);
+
+	for (auto& list : mWaypointChannels)
+	{
+		list.clear();
+
+		list.push_back(zeroVec);
+		list.push_back(oneVec);
+	}
+}
+
 bool KeyframeWindow::updateInput(egpMouse* m, egpKeyboard* key)
 {
+	if (egpKeyboardIsKeyPressed(key, 'y'))
+		resetKeyframes();
+
+	if (egpKeyboardIsKeyPressed(key, ' '))
+		mIsPaused = !mIsPaused;
+
+	//Change our "mode"
 	for (unsigned char c = '1', i = 0; i <= NUM_OF_CHANNELS; ++c, ++i)
 	{
 		if (egpKeyboardIsKeyPressed(key, c))
 			mCurrentChannel = static_cast<KeyframeChannel>(i);
 	}
 
-	if (egpKeyboardIsKeyPressed(key, 'y'))
-	{
-		resetKeyframes();
-	}
-
-	if (egpKeyboardIsKeyPressed(key, ' '))
-		mIsPaused = !mIsPaused;
-
 	cbmath::vec4 mousePos(egpMouseX(m), mWindowSize.y - egpMouseY(m), 0.0f, 1.0f);
 	mousePos = mOnScreenMatrixInv * mousePos;
 
+	//Return false if we're off the quad; it means we should move the camera instead of drawing keyframes
 	if (mousePos.x / mWindowSize.x > 1.0f || mousePos.y / mWindowSize.y > 1.0f)
 		return false;
 
+	//If the mouse is pressed down and we're in "NUM_OF_CHANNELS" mode, scrub the keyhead
 	if (egpMouseIsButtonDown(m, 0) && mCurrentChannel == NUM_OF_CHANNELS)
 	{
 		mCurrentTime = (mousePos.x / mWindowSize.x) * 2.0f;
 		return true;
 	}
 
+	//If the mouse is pressed and we're in a channel mode, add a keyframe 
 	if (egpMouseIsButtonPressed(m, 0) && mCurrentChannel != NUM_OF_CHANNELS)
 	{
 		size_t insertIndex;
@@ -91,6 +96,7 @@ bool KeyframeWindow::updateInput(egpMouse* m, egpKeyboard* key)
 
 void KeyframeWindow::update(float deltaT)
 {
+	//tick tock
 	if (mIsPaused)
 		return;
 
@@ -103,6 +109,7 @@ void KeyframeWindow::update(float deltaT)
 
 void KeyframeWindow::updateWindowSize(float viewport_tw, float viewport_th, float tmpNF, float win_w, float win_h)
 {
+	//Copy-pasted from the in-class example code.
 	mLittleBoxWindowMatrix = cbmath::m4Identity;
 	mLittleBoxWindowMatrix.m00 = 2.0f / viewport_tw;
 	mLittleBoxWindowMatrix.m11 = 2.0f / viewport_th;
@@ -113,6 +120,7 @@ void KeyframeWindow::updateWindowSize(float viewport_tw, float viewport_th, floa
 	mWindowSize.set(win_w, win_h);
 	mOnScreenMatrix = cbmath::makeTranslation4(-0.595f, -0.59f, 0.0f) * cbmath::makeScale4(0.4f);
 
+	//Use a handy glu function to invert the transformation matrix so that we can get mouse clicks
 	if (!gluInvertMatrix(mOnScreenMatrix.m, mOnScreenMatrixInv.m))
 		throw std::invalid_argument("I have no idea how this is possible, but our on-screen transformation matrix could not be inverted!");
 
@@ -130,6 +138,8 @@ float KeyframeWindow::getValAtCurrentTime(KeyframeChannel c)
 	else if (list.size() == 1)
 		return list[0].y;
 
+	//If we have at least 2 positions, loop through the list and find 
+	//the ones that are to the left and to the right of current time.
 	float leftXTime, rightXTime;
 	vec4 posToLeft, posToRight;
 
@@ -146,15 +156,13 @@ float KeyframeWindow::getValAtCurrentTime(KeyframeChannel c)
 		}
 	}
 
+	//Laura figured out this math because she is egod of time
 	float t = (mCurrentTime - leftXTime) / (rightXTime - leftXTime);
 	return egpfwLerp(posToLeft.y, posToRight.y, t) * 2.0f / mWindowSize.y - 1.0f;
 }
 
 void KeyframeWindow::renderToFBO(int* curveUniformSet, int* solidColorUniformSet)
 {	
-	//const cbmath::vec4 waypointColor(1.0, 0.5f, 0.0f, 1.0f);
-	const cbmath::vec4 objectColor(1.0f, 1.0f, 0.5f, 1.0f);
-
 	int j;
 	cbmath::vec4 *waypointPtr;
 	cbmath::mat4 waypointMVP;
@@ -205,8 +213,8 @@ void KeyframeWindow::renderToFBO(int* curveUniformSet, int* solidColorUniformSet
 		}
 	}
 
+	//Drew all the keyframes. Now, draw the scrub head thing.
 	cbmath::vec4 points[2] = { cbmath::vec4(mCurrentTime / 2.0f * mWindowSize.x, 0.0f, 0.0f, 1.0f), cbmath::vec4(mCurrentTime / 2.0f * mWindowSize.x, mWindowSize.y, 0.0f, 1.0f) };
-	
 	egpActivateProgram(mProgramList + drawCurveProgram);
 	egpSendUniformFloatMatrix(curveUniformSet[unif_mvp], UNIF_MAT4, 1, 0, mLittleBoxWindowMatrix.m);
 
@@ -219,6 +227,7 @@ void KeyframeWindow::renderToFBO(int* curveUniformSet, int* solidColorUniformSet
 	egpActivateVAO(mVAOList + pointModel);
 	egpDrawActiveVAO();
 
+	//Now, draw the x axis.
 	points[0] = cbmath::vec4(0.0f, mWindowSize.y / 2.0f, 0.0f, 1.0f);
 	points[1] = cbmath::vec4(mWindowSize.x, mWindowSize.y / 2.0f, 0.0f, 1.0f);
 
@@ -238,11 +247,15 @@ void KeyframeWindow::renderToFBO(int* curveUniformSet, int* solidColorUniformSet
 
 void KeyframeWindow::renderToBackbuffer(int* textureUniformSet)
 {
+	//Draw our FBO to the backbuffer
 	egpActivateProgram(mProgramList + testTextureProgramIndex);
 	egpActivateVAO(mVAOList + fsqModel);
 	egpfwBindColorTargetTexture(mFBOList + curvesFBO, 0, 0);
 	egpSendUniformFloatMatrix(textureUniformSet[unif_mvp], UNIF_MAT4, 1, 0, mOnScreenMatrix.m);
 	egpDrawActiveVAO();
+
+	//Draw the axes using immediate mode :( :( :(
+	//Seriously though, how do you even do this with the programmable pipeline???
 
 	glBegin(GL_BITMAP); //more like GL_BUTTMAP hahahaha
 	egpActivateProgram(0);
@@ -253,48 +266,54 @@ void KeyframeWindow::renderToBackbuffer(int* textureUniformSet)
 		color = cbmath::vec4(0, 0, 0, 0);
 
 	glColor3f(color.r, color.g, color.b);
-
 	glDisable(GL_LIGHTING);
 
-	glWindowPos2i(5, mWindowSize.y * 0.41f);
-	glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '0');
-
-	glWindowPos2i(mWindowSize.x * 0.4f * 0.5f, mWindowSize.y * 0.41f);
-	glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '1');
-
-	glWindowPos2i(mWindowSize.x * 0.4f - 5, mWindowSize.y * 0.41f);
-	glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '2');
-
-	std::string stringToWrite[3];
-
-	if ((int)mCurrentChannel < (int)CHANNEL_ROT_X)
+	//Draw the x axis on top of the box.
 	{
-		stringToWrite[0] = "-5";
-		stringToWrite[1] = "0";
-		stringToWrite[2] = "5";
-	}
-	else if ((int)mCurrentChannel < (int)NUM_OF_CHANNELS)
-	{
-		stringToWrite[0] = "-360";
-		stringToWrite[1] = "0";
-		stringToWrite[2] = "360";
-	}
-	else
-	{
-		return;
+		glWindowPos2i(5, mWindowSize.y * 0.41f);
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '0');
+
+		glWindowPos2i(mWindowSize.x * 0.4f * 0.5f, mWindowSize.y * 0.41f);
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '1');
+
+		glWindowPos2i(mWindowSize.x * 0.4f - 5, mWindowSize.y * 0.41f);
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '2');
 	}
 
-	glWindowPos2i(mWindowSize.x * 0.4f + 5, 5);
-	for (auto i = 0; i < stringToWrite[0].size(); i++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, stringToWrite[0][i]);
+	//Draw the y axis to the side of the box.
+	{
+		std::string stringToWrite[3];
 
-	glWindowPos2i(mWindowSize.x * 0.4f + 5, mWindowSize.y * 0.4f * 0.5f);
-	for (auto i = 0; i < stringToWrite[1].size(); i++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, stringToWrite[1][i]);
+		//Choose which text to use based on the current channel
+		if ((int)mCurrentChannel < (int)CHANNEL_ROT_X)
+		{
+			stringToWrite[0] = "-5";
+			stringToWrite[1] = "0";
+			stringToWrite[2] = "5";
+		}
+		else if ((int)mCurrentChannel < (int)NUM_OF_CHANNELS)
+		{
+			stringToWrite[0] = "-360";
+			stringToWrite[1] = "0";
+			stringToWrite[2] = "360";
+		}
+		else
+		{
+			return;
+		}
 
-	glWindowPos2i(mWindowSize.x * 0.4f + 5, mWindowSize.y * 0.4f - 15);
-	for (auto i = 0; i < stringToWrite[2].size(); i++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, stringToWrite[2][i]);
+		glWindowPos2i(mWindowSize.x * 0.4f + 5, 5);
+		for (auto i = 0; i < stringToWrite[0].size(); i++)
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, stringToWrite[0][i]);
+
+		glWindowPos2i(mWindowSize.x * 0.4f + 5, mWindowSize.y * 0.4f * 0.5f);
+		for (auto i = 0; i < stringToWrite[1].size(); i++)
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, stringToWrite[1][i]);
+
+		glWindowPos2i(mWindowSize.x * 0.4f + 5, mWindowSize.y * 0.4f - 15);
+		for (auto i = 0; i < stringToWrite[2].size(); i++)
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, stringToWrite[2][i]);
+	}
 
 	glEnd();
 }
